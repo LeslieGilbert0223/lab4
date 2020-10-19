afd8d545c400c23144705f6e1a343d4bb8544f8f
lesliegilbert0223 <lesliegilbert0223@gmail.com>
2020-10-19 15:23:47 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict"; 
+
+//name (string)
+let fullName = "Leslie Gilbert";
+
+//age (number)
+let age = "33";
+
+//birthday (string)
+let birthday = "May 2";
+
+//detroitGC (boolean) - choose either true or false
+let detroitGC = true;
+
+//lifeEvents (array with 4 items. 4 important life events)
+let lifeEvents = ["I was born in Fort Myers, FL", "I went to Ferris State and IADT", "I was an EMT for 9 years", "I have a daughter, Savannah, who is 9"]
+
+
+if(detroitGC === true) {
+    console.log(`My name is ${fullName} and I am a student at Grand Circus in Detroit, Michigan. I am currently ${age} years old and my birthday is on ${birthday}`)
+}
